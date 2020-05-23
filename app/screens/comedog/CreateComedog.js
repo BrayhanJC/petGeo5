7c7a8c73453ef1f227a63cdb6889c95c827edb28
Juan Camilo Zuluaga Serna <juancamilozuluagaserna@hotmail.com>
2020-05-23 17:19:37 -0500

    Agregando cambios

--- app/screens/comedog/CreateComedog.js
+++ app/screens/comedog/CreateComedog.js
@@ -1,27 +1,25 @@
-import React, { useState, useRef} from 'react'
-import {  View, Text} from 'react-native'
-import Toast from 'react-native-easy-toast'
-import Loading from '../../components/Loading'
+import React, { useState, useRef } from "react";
+import { View, Text } from "react-native";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/Loading";
 
-import CreateComedogForm from '../../components/comedog/CreateComedogForm'
+import CreateComedogForm from "../../components/comedog/CreateComedogForm";
 
-function CreateComedog (props) {
-    const { navigation } = props
-    const [isLoading, setIsLoading] = useState(false)
-    const toastRef = useRef()
-    return (
-        <View>
-
-            <CreateComedogForm 
-            toastRef={toastRef}
-            setIsLoading={setIsLoading}
-            navigation={navigation}
-            />
-            <Toast ref={toastRef} position='center' opacity={0.9}/>
-            <Loading isVisible={isLoading} text="Creando Comedog"/>
-
-        </View>
-    )
+function CreateComedog(props) {
+  const { navigation } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const toastRef = useRef();
+  return (
+    <View>
+      <CreateComedogForm
+        toastRef={toastRef}
+        setIsLoading={setIsLoading}
+        navigation={navigation}
+      />
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+      <Loading isVisible={isLoading} text="Creando Comedog" />
+    </View>
+  );
 }
 
-export default CreateComedog
+export default CreateComedog;
